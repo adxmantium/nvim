918c84405704f0839f63828837de396565433ebe
adxmantium <ajadams10@gmail.com>
2024-01-24 16:13:51 -0800

    adding new keybinds for telescopes git commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -35,5 +35,7 @@ return {
 		keymap.set("n", "<leader>,", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>/", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>gc", "<cmd>Telescope git_commit<cr>", { desc = "Find string under cursor in cwd" })
 	end,
 }
