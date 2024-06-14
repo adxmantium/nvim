8c460e41713545e1ed400d264f28cd53a45b449e
adxmantium <ajadams10@gmail.com>
2024-06-13 17:04:22 -0700

    feat: changing oil keybind to open as a float

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,14 @@
 return {
 	"stevearc/oil.nvim",
 	keys = {
-		{ "<leader>oo", "<CMD>Oil<CR>", desc = "Open parent directory" },
+		{ "<leader>oo", "<CMD>Oil --float<CR>", desc = "Open parent directory" },
 	},
-	opts = {},
+	opts = {
+		float = {
+			-- Padding around the floating window
+			padding = 20,
+		},
+	},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
