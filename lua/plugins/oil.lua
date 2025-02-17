eb9a6acfad00a93e55b249fb6241c413f7b57cdb
adxmantium <ajadams10@gmail.com>
2025-02-16 23:37:48 -0800

    feat: adding new keybind to open oil with - and modifying C-v to open file in vertical mode to match fzf keybind

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,20 @@
 return {
-	"stevearc/oil.nvim",
-	keys = {
-		{ "<leader>oo", "<CMD>Oil --float<CR>", desc = "Open parent directory" },
-	},
-	opts = {
-		float = {
-			-- Padding around the floating window
-			padding = 20,
-		},
-	},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+  "stevearc/oil.nvim",
+  keys = {
+    { "<leader>oo", "<CMD>Oil --float<CR>", desc = "Open parent directory" },
+    { "-",          "<CMD>Oil --float<CR>", desc = "Open parent directory" },
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+      },
+      float = {
+        -- Padding around the floating window
+        padding = 20,
+      },
+    })
+  end,
 }
